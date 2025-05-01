98c74ee8eb740a5b1ba8491a6ac1a435aeed39d5
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-05-01 15:51:15 +0300

    Enhance database schema by adding home_data table with temperature, humidity, and created timestamp fields

--- website/src/lib/server/db/schema.ts
+++ website/src/lib/server/db/schema.ts
@@ -1,6 +1,13 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+import { sqliteTable, text, integer, real, SQLiteTimestamp } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', {
+// export const user = sqliteTable('user', {
+// 	id: integer('id').primaryKey(),
+// 	age: integer('age')
+// });
+export const home_data = sqliteTable('home_data', {
 	id: integer('id').primaryKey(),
-	age: integer('age')
+	temperature: real('temperature').notNull(),
+	humidity: real('humidity').notNull(),
+	created: text("created").default(sql`(CURRENT_TIMESTAMP)`),
 });
